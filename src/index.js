f6718f13cb6af57598cac1a74afad9d685939722
Guadalupe González López <Guadalupe@MacBook-Air-de-Guadalupe.local>
2019-06-11 15:56:40 -0500

    Se han realizado cambios en el fondo de pantalla, como el color. De igual forma se cambiaron los tipos de letra del encabezado y de los botones.

--- src/index.js
+++ src/index.js
@@ -2,9 +2,27 @@ const buttonchat = document.getElementById('button_chat');
 const firstsection = document.getElementById ('first_section');
 const secondsection =document.getElementById('second_section');
 
+
 const getfirstsection = ()=> {
 firstsection.classList.add ("disappear");
 secondsection.classList.remove("appear");
-}
+};
 
 buttonchat.addEventListener ("click", getfirstsection);
+
+
+const buttoncifrar = document.getElementById('button_cifrar');
+const buttondescifrar = document.getElementById('button_descifrar');
+const input = document.getElementById('input');
+
+
+
+const alfabeto = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+clave = "5";
+newmensaje="";
+mensaje=input("Introduce tu mensaje");
+word= input ("Introduce tu mensaje:");
+position=alfabeto.find (input);
+newposition = (position + clave) %27;
+newword= alfabeto [newposition];
+print (newword);
