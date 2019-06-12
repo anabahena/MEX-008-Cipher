7611b00131a9f86fbd4fd7ce5a6d0a7f64b52596
Guadalupe González López <Guadalupe@MacBook-Air-de-Guadalupe.local>
2019-06-12 14:19:02 -0500

    Se ha agregado código para la función cifrar

--- src/cipher.js
+++ src/cipher.js
@@ -1,3 +1,28 @@
 window.cipher = {
-  // ... 
+  /*... cipher.encode(offset, string): offset es el número de posiciones que queremos 
+  mover a la derecha en el alfabeto y string el mensaje (texto) que queremos cifrar.
+cipher.decode(offset, string): offset es el número de posiciones que queremos mover 
+a la izquierda en el alfabeto y string el mensaje (texto) que queremos descifrar.*/
+
+/*Codificar el contenido que se dirija en la caja*/
+encode (offset, string) => {
+  /*Declaración de variable para conocer el tamaño del string ingresado*/ 
+  let length = string.length; 
+ /*Declaración de variable offset, convirtiendola en número entero con el parseInt*/
+  let offSetConv=parseInt (offset);
+  /*Declaración de variable donde se guardará el mensaje ingresado en formato ASCII*/
+  let inputMensage;
+    /*Declaración de variable donde se guardará el mensaje ASCII con la función de cifrado*/
+  let cipherMensage;
+/*Evaluar la necesidad de esta variable*/
+  let ascciEncrypted="";
+
+  
+
+
+
+}
+
+/*(position+wordinput)%26 Formula para obtener los desplazamientos*/
+
 };
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const firstsection = document.getElementById ('first_section');
 const secondsection =document.getElementById('second_section');
 
 
+
 const getfirstsection = ()=> {
 firstsection.classList.add ("disappear");
 secondsection.classList.remove("appear");
@@ -11,18 +12,11 @@ secondsection.classList.remove("appear");
 buttonchat.addEventListener ("click", getfirstsection);
 
 
+
+
+
+
 const buttoncifrar = document.getElementById('button_cifrar');
 const buttondescifrar = document.getElementById('button_descifrar');
 const input = document.getElementById('input');
 
-
-
-const alfabeto = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-clave = "5";
-newmensaje="";
-mensaje=input("Introduce tu mensaje");
-word= input ("Introduce tu mensaje:");
-position=alfabeto.find (input);
-newposition = (position + clave) %27;
-newword= alfabeto [newposition];
-print (newword);
