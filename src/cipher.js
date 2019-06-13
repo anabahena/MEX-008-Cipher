2334186b7355d006dc0acf92c79a74dac064ee1c
Guadalupe González López <Guadalupe@MacBook-Air-de-Guadalupe.local>
2019-06-13 18:36:43 -0500

    Se ha realizado la función para cifrar, sin embargo aún no concatena, hay que revisar +=

--- src/cipher.js
+++ src/cipher.js
@@ -1,35 +1,35 @@
 window.cipher = {
-  /*... cipher.encode(offset, string): offset es el número de posiciones que queremos 
-  mover a la derecha en el alfabeto y string el mensaje (texto) que queremos cifrar.
-cipher.decode(offset, string): offset es el número de posiciones que queremos mover 
-a la izquierda en el alfabeto y string el mensaje (texto) que queremos descifrar.*/
-
-
 
 /*Codificar el contenido que se dirija en la caja*/
-const encode = (offset, string) {
+encode : (offset, string) => {
   /*Declaración de variable para conocer el tamaño del string ingresado*/ 
   let size = string.length; 
  /*Declaración de variable offset, convirtiendola en número entero con el parseInt*/
-  let offSetConv=parseInt (offset);
+  let offSetConv= parseInt (offset);
   /*Declaración de variable donde se guardará el mensaje ingresado en formato ASCII*/
-  let inputMensage;
+  let asciiMensage;
     /*Declaración de variable donde se guardará el mensaje ASCII con la función de cifrado*/
   let cipherMensage;
 /*Evaluar la necesidad de esta variable*/
   let mensageEncrypted="";
-
-  for (let i=0 i<size; i++){
-    inputMensage=string.charCodeAt(i)
-
-      cipherMensage =(inputMensage-65+offSetConv)%26+65;
-      mensageEncrypted=mensageEncrypted+string.from.charCodeAt(cipherMensage);
+//Realiza un bucle del tamaño de el texto ingresado
+  for (let i=0; i<size; i++){ 
+// Devuelve el índice de la cadena introducida
+    asciiMensage=string.charCodeAt(i);
+// Aplicando fórmula de cifrado César
+    cipherMensage= (asciiMensage-65+offSetConv)%26+65;
+// Obteniendo los caracteres cifrados de forma concatenada, para cifrar la frase completa
+    const result = mensageEncrypted + String.fromCharCode (cipherMensage);
+    console.log(result)
   }
-return mensageEncrypted
+
+// return (cipher.encode.result);
   
+  },
+
 }
 
-/*(position+wordinput)%26 Formula para obtener los desplazamientos*/
+decode:(offset, string) => {
 
-};
-module.exports = encode;
+
+}
--- src/index.js
+++ src/index.js
@@ -1,23 +1,42 @@
+
+// Declaración de variables  de secciones y botones
 const buttonchat = document.getElementById('button_chat');
 const firstsection = document.getElementById ('first_section');
 const secondsection =document.getElementById('second_section');
+const buttoncifrar = document.getElementById('button_cifrar');
+const buttondescifrar = document.getElementById('button_descifrar');
 
 
 
-
+// Funcion para desaparecer la primera sección y aparecer la segunda sección
 const getfirstsection = ()=> {
 firstsection.classList.add ("disappear");
 secondsection.classList.remove("appear");
 };
 
+// Invocación del boton para realizar lo que se declara en la funcion getfirstsection
 buttonchat.addEventListener ("click", getfirstsection);
 
 
 
+buttoncifrar.addEventListener ("click", () => {
+    const offsetCif= document.getElementById("offsetCif");
+    const inputStringCif = document.getElementById ("inputStringCif");
+
+    window.cipher.encode(offsetCif.value, inputStringCif.value);
+});
 
 
 
-const buttoncifrar = document.getElementById('button_cifrar');
-const buttondescifrar = document.getElementById('button_descifrar');
-const input = document.getElementById('input');
+
+
+buttondescifrar.addEventListener ("click", () => {
+    const offsetDescif= document.getElementById("offsetDescif");
+    const inputStringDescif = document.getElementById ("inputStringDescif");
+
+    window.cipher.encode(offsetDescif.value, output.value);
+});
+
+
+
 
